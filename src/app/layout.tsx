56abe77a8f69dcf1061f3eb12ef7aa0a07da5d87
JonMoore9000 <jonmoore9000@gmail.com>
2025-04-25 09:39:59 -0600

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "../context/AuthContext"
+import { AuthProvider } from "../context/AuthContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
     <html lang="en">
   <body className={`${nunito.className} antialiased`}>
     <AuthProvider>{children}</AuthProvider>
+    <Analytics />
   </body>
 </html>
 
